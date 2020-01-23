7bb0044315e912f3cc124370e3f648a38a693c4b
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-23 15:30:20 -0300

    novos exercícios resolvidos

---
+++ capitulo 3/isParImpar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*
+ * Fazer um programa para ler um número inteiro e dizer se este número é par ou impar
+*/
+namespace isParImpar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double isParImpar = double.Parse(Console.ReadLine());
+      if (isParImpar % 2 == 0)
+      {
+        Console.WriteLine("PAR");
+      }
+      else
+      {
+        Console.WriteLine("IMPAR");
+      }
+    }
+  }
+}
---
+++ capitulo 3/saoMultiplos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+/* 
+URI Online Judge | 1044
+Múltiplos
+Adaptado por Neilor Tonin, URI  Brasil
+
+Timelimit: 1
+Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos", indicando se os valores lidos são múltiplos entre si.
+
+Entrada
+A entrada contém valores inteiros.
+
+Saída
+A saída deve conter uma das mensagens conforme descrito acima.
+
+Exemplo de Entrada	Exemplo de Saída
+6 24
+
+Sao Multiplos
+
+6 25
+
+Nao sao Multiplos
+*/
+namespace saoMultiplos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string[] input = Console.ReadLine().Split(' ');
+      short A, B;
+      A = short.Parse(input[0]);
+      B = short.Parse(input[1]);
+      if ((A % B == 0) || (B % A == 0))
+      {
+        Console.WriteLine("Sao Multiplos");
+      }
+      else
+      {
+        Console.WriteLine("Nao sao Multiplos");
+      }
+    }
+  }
+}
