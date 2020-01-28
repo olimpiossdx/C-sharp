1d4d772954fd0947c6b9360df20b08d8ebd61e18
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-27 21:12:55 -0300

    operadores logicos

---
+++ capitulo 3/programs/operadoreslogicos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace operadoreslogicos {
+    class Program {
+        static void Main (string[] args) {
+            bool c1 = 2 > 3 || 4 != 5; //true
+            bool c2 = !(2 > 3) && 4 != 5; //true
+            Console.WriteLine (c1);
+            Console.WriteLine (c2);
+            Console.WriteLine ("----------------------");
+
+            bool c3 = 10 < 5;
+            bool c4 = c1 || c2 && c3;
+            Console.WriteLine (c3);
+            Console.WriteLine (c4);
+
+        }
+    }
+}
