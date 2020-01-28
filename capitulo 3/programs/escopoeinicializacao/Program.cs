13c64b575eede901c91384421a623e90c4240b1e
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-27 22:16:44 -0300

    escopo e incialização

---
+++ capitulo 3/programs/escopoeinicializacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace escopoeinicializacao {
+    class Program {
+        static void Main (string[] args) {
+            // variavel precisa ser inicializada
+            int x = 10;
+            Console.WriteLine (x);
+
+            // erro por causa de declração da variável fora do escopo princial da aplicação; Desconto pertence ao escopo do if()
+            double preco = double.Parse (Console.ReadLine ());
+            double desconto;
+            if (preco > 100.0) {
+                //double desconto = preco * 0.1;
+                desconto = preco * 0.1;
+            }
+            Console.WriteLine (desconto);
+        }
+    }
+}
