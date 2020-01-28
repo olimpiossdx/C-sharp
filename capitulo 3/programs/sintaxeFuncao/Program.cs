9e484d00ea2958042d3ad527403c0cf18206504b
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-27 22:34:25 -0300

    função e sintaxe[básico]

---
+++ capitulo 3/programs/sintaxeFuncao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace sintaxeFuncao {
+  class Program {
+    public static int Maior (int n1, int n2, int n3) {
+      int m;
+      if (n1 > n2 && n1 > n3) {
+        m = n1;
+      } else if (n2 > n3) {
+        m = n2;
+      } else {
+        m = n3;
+      }
+      return m;
+    }
+    static void Main (string[] args) {
+      //Principais vantagens: modularização  delegação e reaproveitamento
+      Console.WriteLine ("Digite 3 números");
+      int n1 = int.Parse (Console.ReadLine ());
+      int n2 = int.Parse (Console.ReadLine ());
+      int n3 = int.Parse (Console.ReadLine ());
+      double resultado = Maior (n1, n2, n3);
+      Console.WriteLine ("Maior = " + resultado);
+    }
+  }
+}
