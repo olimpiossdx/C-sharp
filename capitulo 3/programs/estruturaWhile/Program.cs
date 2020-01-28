08ddad4650f71778999ae40b7f0b94f48071df29
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-28 20:34:09 -0300

    exemplos de estrutura while

---
+++ capitulo 3/programs/estruturaWhile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace estruturaWhile {
+  class Program {
+    static void Main (string[] args) {
+      Console.Write ("Digite um número: ");
+      double number = double.Parse (Console.ReadLine ());
+      while (number >= 0) {
+        Console.WriteLine ($"{Math.Sqrt(number):F3}");
+        Console.Write ("Digite um número: ");
+        number = double.Parse (Console.ReadLine ());
+      }
+      Console.WriteLine ("Número negativo!");
+    }
+  }
+}
