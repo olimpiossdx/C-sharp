4be09c00eca9ce9034cb42f742b35861d28f0692
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-28 22:56:25 -0300

    correção de "erro"

--- capitulo 3/programs/escopoeinicializacao/Program.cs
+++ capitulo 3/programs/escopoeinicializacao/Program.cs
@@ -9,7 +9,7 @@ namespace escopoeinicializacao {
 
             // erro por causa de declração da variável fora do escopo princial da aplicação; Desconto pertence ao escopo do if()
             double preco = double.Parse (Console.ReadLine ());
-            double desconto;
+            double desconto = 0;
             if (preco > 100.0) {
                 //double desconto = preco * 0.1;
                 desconto = preco * 0.1;
