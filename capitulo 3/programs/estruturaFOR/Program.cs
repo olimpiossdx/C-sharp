0b05f0d246b8a05fb1237496ff3f735835fad59c
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-28 21:46:32 -0300

    estutura for

---
+++ capitulo 3/programs/estruturaFOR/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace estruturaFOR {
+  class Program {
+    static void Main (string[] args) {
+      Console.Write ("Quantos números inteiros você vai digitar: ");
+      int n = int.Parse (Console.ReadLine ());
+      int soma = 0;
+      for (int i = 0; i < n; i++) {
+        Console.Write ($"Valor #{i+1}: ");
+        soma += int.Parse (Console.ReadLine ());
+      }
+      Console.WriteLine ($"Soma = {soma}");
+    }
+  }
+}
