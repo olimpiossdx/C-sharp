530aa79778d479b8144390206314eacdcafcd4be
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-04 20:22:52 -0300

    adicionando classe estática

---
+++ capitulo 4/circunferenciaVolumeEsfera/Calculadora.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace circunferenciaVolumeEsfera {
+  class Calculadora {
+    public static double pi = 3.14;
+
+    public static double Circunferencia (double r) {
+      return 2 * pi * r;
+    }
+    public static double Volume (double r) {
+      return ((4.0 / 3.0) * pi * r * r * r);
+    }
+  }
+}
--- capitulo 4/circunferenciaVolumeEsfera/Program.cs
+++ capitulo 4/circunferenciaVolumeEsfera/Program.cs
@@ -3,19 +3,14 @@
 namespace circunferenciaVolumeEsfera {
   class Program {
     static double raio;
-    static double pi = 3.14;
     static void Main (string[] args) {
+   
       Console.Write ("Entre com valor do raio: ");
       raio = double.Parse (Console.ReadLine ());
-      Console.WriteLine ($"Circunferência: {Circunferencia(raio):F2}");
-      Console.WriteLine ($"Volume: {Volume(raio):F2}");
-      Console.WriteLine ($"Valor de Pi: {Math.PI:F2}");
+      Console.WriteLine ($"Circunferência: {Calculadora.Circunferencia(raio):F2}");
+      Console.WriteLine ($"Volume: {Calculadora.Volume(raio):F2}");
+      Console.WriteLine ($"Valor de Pi: {Calculadora.pi:F2}");
     }
-    static double Circunferencia (double r) {
-      return 2 * pi * r;
-    }
-    static double Volume (double r) {
-      return ((4.0 / 3.0) * pi * r * r * r);
-    }
+    
   }
 }
