bf0ace5fd0049b78de9e15851c9f55f20968d5e6
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-04 21:53:15 -0300

    ecapsulamento

--- capitulo 5/dadosProduto/Produto.cs
+++ capitulo 5/dadosProduto/Produto.cs
@@ -2,31 +2,47 @@ using System;
 
 namespace dadosProduto {
   class Produto {
-    public string Nome;
-    public double Preco;
-    public double Quantidade;
+    private string _nome;
+    private double _preco;
+    private double _quantidade;
     public Produto () {
       Quantidade = 0;
     }
     public Produto (string nome, double preco) : this () {
-      Nome = nome;
-      Preco = preco;
+      _nome = nome;
+      _preco = preco;
     }
     public Produto (string nome, double preco, int quantidade) : this (nome, preco) {
-      Quantidade = quantidade;
+      _quantidade = quantidade;
+    }
+
+    public GetNome () {
+      return _nome;
+    }
+    public SetNome (string nome) {
+      if (nome != null && nome.Length > 3) {
+        _nome = nome;
+      }
+    }
+    public GetPreco () {
+      return _preco;
+    }
+
+    public GetQuantidade () {
+      return _quantidade;
     }
 
     public double ValorTotalEmEstoque () {
-      return Quantidade * Preco;
+      return _quantidade * Preco;
     }
     public void AdicionarProdutos (int quantidade) {
-      Quantidade += quantidade;
+      _quantidade += quantidade;
     }
     public void RemoverProdutos (int quantidade) {
-      Quantidade -= quantidade;
+      _quantidade -= quantidade;
     }
     public override string ToString () {
-      return $"{Nome}, $ {Preco:F2}, {Quantidade} unidade(s), Total: $ {ValorTotalEmEstoque():F2}";
+      return $"{_nome}, $ {_preco:F2}, {_quantidade} unidade(s), Total: $ {ValorTotalEmEstoque():F2}";
     }
 
   }
