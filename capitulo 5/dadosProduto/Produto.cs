d348567c16e321a8e76a94b4d9627e9ef1fe7d0e
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-04 21:22:04 -0300

    sobrecarga

--- capitulo 5/dadosProduto/Produto.cs
+++ capitulo 5/dadosProduto/Produto.cs
@@ -6,6 +6,24 @@ namespace dadosProduto {
     public double Preco;
     public double Quantidade;
 
+    public Produto () {
+
+    }
+    public Produto (string nome, double preco, double quantidade) {
+      Nome = nome;
+      Preco = preco;
+      Quantidade = quantidade;
+    }
+    public Produto (string nome, double preco, double quantidade) {
+      Nome = nome;
+      Preco = preco;
+      Quantidade = quantidade;
+    }
+    public Produto (string nome, double preco) {
+      Nome = nome;
+      Preco = preco;
+      Quantidade = 0;
+    }
     public Produto (string nome, double preco, double quantidade) {
       Nome = nome;
       Preco = preco;
