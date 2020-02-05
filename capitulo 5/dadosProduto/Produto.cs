a33ac02592a46d7bed87d86da5e7ecf6c12e09a8
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-04 21:41:08 -0300

    palavra this

--- capitulo 5/dadosProduto/Produto.cs
+++ capitulo 5/dadosProduto/Produto.cs
@@ -5,10 +5,15 @@ namespace dadosProduto {
     public string Nome;
     public double Preco;
     public double Quantidade;
-    public Produto () { }
-    public Produto (string nome, double preco) {
+    public Produto () {
+      Quantidade = 0;
+    }
+    public Produto (string nome, double preco) : this () {
       Nome = nome;
       Preco = preco;
+    }
+    public Produto (string nome, double preco, int quantidade) : this (nome, preco) {
+      Quantidade = quantidade;
     }
 
     public double ValorTotalEmEstoque () {
