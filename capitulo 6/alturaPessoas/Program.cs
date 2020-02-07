af8a0a7e9e74c3b43d90ba3b126d2264a800dcf0
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-06 21:48:11 -0300

    vector parte 1

---
+++ capitulo 6/alturaPessoas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Fazer um programa  para ler  um número  inteiro N e altura  de N pessoas.
+// Armazene as N alturas em um vetor. Em seguida, mostrar a altura média dessas pessoas.
+
+namespace alturaPessoas {
+  class Program {
+    static void Main (string[] args) {
+      Console.Write ("Entre com número da quantidade de alturas a serem lidas: ");
+      int n = int.Parse (Console.ReadLine ());
+      double[] vector = new double[n];
+      double sum = 0.0;
+
+      for (int i = 0; i < n; i++)
+        vector[i] = double.Parse (Console.ReadLine ());
+
+      for (int i = 0; i < n; i++)
+        sum += vector[i];
+
+      Console.WriteLine ($"AVERAGE HEIGHT: {sum:f2}");
+
+    }
+  }
+}
