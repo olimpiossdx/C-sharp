8494f70b250ade47f3e62fec3fbc26b68dd736e0
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-06 22:25:02 -0300

    exemplo com vetor part 2

---
+++ capitulo 6/nProdutos/Produto.cs
@@ -0,0 +1,8 @@
+
+
+namespace nProdutos {
+  class Produto {
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+  }
+}
---
+++ capitulo 6/nProdutos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace nProdutos {
+  class Program {
+    static void Main (string[] args) {
+      int n;
+      Produto[] produtos;
+      double soma = 0.0;
+
+      Console.Write ("informe a quantidade de produtos a serem lidos: ");
+      n = int.Parse (Console.ReadLine ());
+
+      produtos = new Produto[n];
+
+      for (int i = 0; i < n; i++)
+        produtos[i] = new Produto { Nome = Console.ReadLine (), Preco = double.Parse (Console.ReadLine ()) };
+
+      for (int i = 0; i < n; i++)
+        soma += produtos[i].Preco;
+        
+      Console.WriteLine ($"AVERAGE HEIGHT: {soma/n}");
+    }
+  }
+}
