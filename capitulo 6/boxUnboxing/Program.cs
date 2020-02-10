a4d50e4ad3bdbfddca6c2d3f1cd02314825e5132
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-09 23:03:47 -0300

    box e unboxing

---
+++ capitulo 6/boxUnboxing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace boxUnboxing {
+  class Program {
+    static void Main (string[] args) {
+      int x = 20;
+      //Box -> processo de conversão de um tipo valor para um objeto tipo referência
+      Object obj = x;
+
+      //Unboxing -> processo de conversão de um objeto tipo referência para um objeto tipo valor compatível
+      int y = (int) obj;
+    }
+  }
+}
