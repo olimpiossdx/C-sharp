3926099251d6d5596c541714f77c884c977b2d8e
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-09 22:02:43 -0300

    modificador de parâmetros: params

---
+++ capitulo 6/calculadoramp/CalcuadoraVect.cs
@@ -0,0 +1,10 @@
+namespace calculadoramp {
+  class CalculadoraVect {
+    public static int Sum (int[] numbers) {
+      int sum = 0;
+      for (int i = 0; i < numbers.Length; i++)
+        sum += numbers[i];
+      return sum;
+    }
+  }
+}
---
+++ capitulo 6/calculadoramp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calculadoramp {
+  /*calculadora utilizando vetor*/
+  class Program {
+    static void Main (string[] args) {
+      int s1 = Calculadora.Sum (2, 3);
+      int s2 = Calculadora.Sum (2, 3, 8);
+      int s3 = Calculadora.Sum (2, 3, 8, 9);
+      int result = CalculadoraVect.Sum (new int[] { 10, 20, 30, 40 });
+
+      // utilizando parâmetros variáveis
+      int result2 = CalculadoraMParams.Sum (10, 20, 30, 40);
+
+    }
+  }
+}
