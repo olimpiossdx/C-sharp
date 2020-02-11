8f94cc610f2f24753345b46c58a47f134fd0625c
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-10 21:22:12 -0300

    laço for e for each

---
+++ capitulo 6/lacoForeach/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lacoForeach {
+  class Program {
+    static void Main (string[] args) {
+      string[] vect = new string[] { "Maria", "Bob", "Alex" };
+      // leitura: para cada string obj contido em vect faça:
+      foreach (string obj in vect)
+        Console.WriteLine (obj);
+
+    }
+  }
+}
