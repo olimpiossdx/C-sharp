0a34dd00b667489214cf39e0ee8e3434fe5efb58
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-12 21:12:06 -0300

    exemplo matriz

---
+++ capitulo 6/matriz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace matriz {
+  class Program {
+    static void Main (string[] args) {
+      Console.WriteLine ("Hello World!");
+
+      double[, ] mat = new double[2, 3];
+
+      Console.WriteLine (mat.Length);
+
+      Console.WriteLine (mat.Rank);
+
+      Console.WriteLine (mat.GetLength (0));
+
+      Console.WriteLine (mat.GetLength (3));
+
+    }
+  }
+}
