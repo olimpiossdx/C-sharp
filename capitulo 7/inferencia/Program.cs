117d9ac0feeecb083d7facce74eb54dd19ebc681
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-13 21:40:28 -0300

    inferência de tipo com var

---
+++ capitulo 7/inferencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace inferencia {
+  class Program {
+    static void Main (string[] args) {
+
+      // utiliza-se var para realizar inferência de tipo
+      var x = 10;
+      var y = 20.0;
+      var z = "Maria";
+
+      //    int w = z;
+
+      Console.WriteLine (x);
+      Console.WriteLine (y);
+      Console.WriteLine (z);
+
+    }
+  }
+}
