48ad4b7bddf3aa583ac74cc9abfc297f76ec4998
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-02-26 20:36:11 -0300

    inicio e atividade capitulo 9

--- capitulo 9/composicao/Program.cs
+++ capitulo 9/composicao/Program.cs
@@ -4,6 +4,15 @@
  Ler os dados de um trabalho com N contratos (N fornecido pelo usuário). 
  Depois, solicitar do usuário um mês e mostrar qual foi o salário do funcionário nesse mês, conforme exemplo:
  
+ ------------------------------------------------------------------------------------------------------------
+ tipo de associação que permite que um objeto contenha outro.
+Relação "tem-um" ou "tem-vários"
+
+vantagens
+.Organização: divisão de responsabilidades
+.Coesão
+.FLexibilidade
+.Reuso
 */
 namespace composicao {
   class Program {
