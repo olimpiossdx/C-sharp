e67c3ba0869a38df9a4d556e40a01343e9f5496f
Olimpio <olimpiodx@gmail.com>
2020-02-26 22:37:58 -0300

    projeto salário de funcionário

--- capitulo 9/composicao/Program.cs
+++ capitulo 9/composicao/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using composicao.Entities;
+using composicao.Entities.Enums;
 
 /*
  Ler os dados de um trabalho com N contratos (N fornecido pelo usuário). 
@@ -17,7 +19,48 @@ vantagens
 namespace composicao {
   class Program {
     static void Main (string[] args) {
-      Console.WriteLine ("Hello World!");
+      short qtdContracts, hoursDuration; 
+      int year, month;
+      string name;
+      Departament departament;
+      double baseSalary;
+      DateTime dateIncome;
+      WorkerLevel workerLevel;
+      Worker worker = new Worker ();
+
+      Console.Write ("Enter departament's name: ");
+      departament = new Departament (Console.ReadLine ());
+      Console.WriteLine ("Enter worker data: ");
+      Console.Write ("Name: ");
+      name = Console.ReadLine ();
+      Console.Write ("Level (junior/MidLevel/Senior): ");
+      workerLevel = Enum.Parse<WorkerLevel> (Console.ReadLine ());
+      Console.Write ("Base salary: ");
+      baseSalary = double.Parse (Console.ReadLine ());
+
+      worker = new Worker (name, workerLevel, baseSalary, departament);
+
+      Console.Write ("\nHow many contracts to this worker? ");
+      qtdContracts = short.Parse (Console.ReadLine ());
+      for (int i = 0; i < qtdContracts; i++) {
+        Console.WriteLine ($"Enter #{i+1} contract data: ");
+        Console.Write ("Date (DD/MM/YY): ");
+        dateIncome = DateTime.Parse (Console.ReadLine ());
+        Console.Write ("Value por hour: ");
+        baseSalary = double.Parse (Console.ReadLine ());
+        Console.Write ("Duration (hours): ");
+        hoursDuration = short.Parse (Console.ReadLine ());
+        worker.AddContract (new HourContract (dateIncome, baseSalary, hoursDuration));
+      }
+
+      Console.Write ("\nEnter month and year to calculate income (MM/YYY): ");
+      dateIncome = DateTime.Parse (Console.ReadLine ());
+      year = int.Parse (dateIncome.Year.ToString());
+      month = int.Parse (dateIncome.Month.ToString());
+      Console.WriteLine ($"Name: {worker.Name}");
+      Console.WriteLine ($"Departament: {worker.Departament}");
+      Console.WriteLine ($"Income for {dateIncome.ToString()} {worker.Income(year,month).ToString("F2")}");
+
     }
   }
 }
