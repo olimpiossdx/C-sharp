8e694558cc5b64fb005be780e0c8f1a6fe247468
Olimpio <olimpiodx@gmail.com>
2020-02-29 19:45:12 -0300

    Herança

---
+++ capitulo 10/heranca/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using heranca.Entities;
+/*
+  Herança é um tipo de associação que permite que uma lcasse herde dados e comportamentos de outra.
+  Definições importantes
+  *Vantagens
+    * Reuso
+    * Polimorfismo
+  *Sintaxe  
+    * : (estende)
+    * base (referência para a superclasse)
+
+*/
+namespace heranca {
+  class Program {
+    static void Main (string[] args) {
+      BusinessAccount account = new BusinessAccount (8010, "Bob champs", 100.00, 500.0);
+      //account.Balance = 200.00 não é possível pelo modificador de acesso!
+      Console.WriteLine (account);
+    }
+  }
+}
