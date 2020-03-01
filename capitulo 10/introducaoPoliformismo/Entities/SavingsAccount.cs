1ab921249efd17ed4e94fe4ea3178b3a605a18b5
Olimpio <olimpiodx@gmail.com>
2020-03-01 09:41:22 -0300

    introdução ao poliformismo

---
+++ capitulo 10/introducaoPoliformismo/Entities/SavingsAccount.cs
@@ -0,0 +1,19 @@
+namespace introducaoPoliformismo.Entities {
+  class SavingAccount : Account {
+    public double InterestRate { get; set; } //taxa de juros
+
+    public SavingAccount () { }
+    public SavingAccount (int number, string holder, double balance, double interestRate) : base (number, holder, balance) {
+      InterestRate = interestRate;
+    }
+    public void UpdateBalance () {
+      Balance += Balance * InterestRate;
+    }
+    public override void Withdraw (double amount) {
+      //Balance -= amount + 2.0;
+      //aproveitando a palavra base 
+      base.Withdraw (amount);
+      Balance -= 2.0;
+    }
+  }
+}
---
+++ capitulo 10/introducaoPoliformismo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using introducaoPoliformismo.Entities;
+/*
+
+  Pilares da OOP (programação orientada a objetos)
+  .Ecapsulamento
+  .Herança
+  .Poliformismo -> recurso que permite que variáveis de um mesmo tipo mais genérfico possam apontar para objetos
+   de tipos específicos diferentes, tendo assim comportamentos diferentes conforme cada toipo específico.
+
+   ex:
+      //Associação de tipo específico com tipo genérico é feita em tempo de execução(upcasting)
+      //O compilador não sabe para qual tipo específico a chamda do método Withdraw está sendo feita (ele só sabe que são duas variáveis do tipo Account)
+      
+      Account acc1 = new Account (1001, "Jujé", 500.00);
+      Account acc2 = new SavingAccount (1000, "Jujéia", 500.00, 0.01);
+
+      acc1.Withdraw (10.0);
+      acc2.Withdraw (10.0);
+
+      Console.WriteLine (acc1.Balance);
+      Console.WriteLine (acc2.Balance);
+
+*/
+namespace introducaoPoliformismo {
+  class Program {
+    static void Main (string[] args) {
+      Account acc1 = new Account (1001, "Jujé", 500.00);
+      Account acc2 = new SavingAccount (1000, "Jujéia", 500.00, 0.01);
+
+      acc1.Withdraw (10.0);
+      acc2.Withdraw (10.0);
+
+      Console.WriteLine (acc1.Balance);
+      Console.WriteLine (acc2.Balance);
+    }
+  }
+}
