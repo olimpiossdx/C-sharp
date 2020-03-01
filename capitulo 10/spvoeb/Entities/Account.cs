98b658a08127840bdb75c0d588e3bdc44cb42939
Olimpio <olimpiodx@gmail.com>
2020-03-01 09:16:23 -0300

    sobreposição, palavras virutal, override e base

---
+++ capitulo 10/spvoeb/Entities/Account.cs
@@ -0,0 +1,20 @@
+namespace spvoeb.Entities {
+  class Account {
+    public int Number { get; private set; }
+    public string Holder { get; private set; }
+    public double Balance { get; protected set; }
+    public Account () { }
+    public Account (int number, string holder, double balance) {
+      Number = number;
+      Holder = holder;
+      Balance = balance;
+    }
+    // virtual -> afirma que o método pode ser sobrescrito ou sobreposto
+    public virtual void Withdraw (double amount) {
+      Balance -= amount + 5.0;
+    }
+    public void Deposit (double amount) {
+      Balance += amount;
+    }
+  }
+}
---
+++ capitulo 10/spvoeb/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using spvoeb.Entities;
+/*
+  Sobreposição ou sobrescrita
+  . É a implementação de um métodod de uma superclasse na subclasse.
+  . Para que um método comum (não abstrato) possa ser sobreposto, deve
+    ser incluído nele o prefixo "virtual"
+  . Ao sobrescrever um método, devemos incluir o prefixo "override"
+
+  Palavra base
+  . É possível chamar a implementaçção da superclasse usando a palavra base.
+
+  Exemplo: suponha que a regra para saque para conta poupança seja realizar o saque
+  normalmente da superclasse (Account), e depois descontar mais 2.0.
+
+  pulbic override void Withdraw (double amount){
+    base.Withdraw(amount);
+    Balance -=2.0;
+  }
+*/
+
+namespace spvoeb {
+  class Program {
+    static void Main (string[] args) {
+      // Console.WriteLine ("Hello World!");
+      Account acc1 = new Account (1001, "Jujé", 500.00);
+      Account acc2 = new SavingAccount (1000, "Jujéia", 500.00, 0.01);
+
+      acc1.Withdraw (10.0);
+      acc2.Withdraw (10.0);
+
+      Console.WriteLine (acc1.Balance);
+      Console.WriteLine (acc2.Balance);
+
+    }
+  }
+}
