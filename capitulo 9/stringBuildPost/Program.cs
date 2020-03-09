a21325b09c553563af616c2504c64a3a450e0066
Olimpio <olimpiodx@gmail.com>
2020-03-08 22:10:33 -0300

    alterando formatação

--- capitulo 9/stringBuildPost/Program.cs
+++ capitulo 9/stringBuildPost/Program.cs
@@ -9,11 +9,7 @@ namespace stringBuildPost {
       c1 = new Comment ("Have a nice trip!");
       c2 = new Comment ("Wow that's awesome!");
 
-      p1 = new Post (
-        DateTime.Parse ("21/06/2018 13:05:44"),
-        "Traveling to New Zeland",
-        "I'm going to visit this wonderful country",
-        12);
+      p1 = new Post (DateTime.Parse ("21/06/2018 13:05:44"), "Traveling to New Zeland", "I'm going to visit this wonderful country", 12);
 
       p1.AddComment (c1);
       p1.AddComment (c2);
@@ -21,18 +17,13 @@ namespace stringBuildPost {
       c1 = new Comment ("Good night");
       c2 = new Comment ("May the Force be with you");
 
-      p2 = new Post (
-        DateTime.Parse ("28/07/2018 23:14:49"),
-        "Good night guys",
-        "See you tomorrow",
-        5);
+      p2 = new Post (DateTime.Parse ("28/07/2018 23:14:49"), "Good night guys", "See you tomorrow", 5);
 
       p2.AddComment (c1);
       p2.AddComment (c2);
 
-      Console.WriteLine(p1.ToString()+"\n");
-      Console.WriteLine(p2.ToString());
-
+      Console.WriteLine (p1.ToString () + "\n");
+      Console.WriteLine (p2.ToString ());
     }
   }
 }
