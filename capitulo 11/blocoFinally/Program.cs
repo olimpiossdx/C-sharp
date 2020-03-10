36ecd63363f190d52c345c741b78aa4dcfc4e6d9
Olimpio <olimpiodx@gmail.com>
2020-03-10 20:37:59 -0300

    bloco finally

---
+++ capitulo 11/blocoFinally/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+/*
+  Bloco finally: é um bloco que contém código a ser executado independentemente de ter ocorrido ou não execução.
+  Exemplço básico: fechar um arquivo ou conexão de banco de dados ao final do processamento.
+*/
+
+namespace blocoFinally {
+  class Program {
+    static void Main (string[] args) {
+      FileStream fs = null;
+      try {
+        fs = new FileStream (@"data.txt", FileMode.Open);
+        StreamReader sr = new StreamReader (fs);
+        string line = sr.ReadLine ();
+      } catch (FileNotFoundException e) {
+        Console.WriteLine (e.Message);
+      } finally {
+        if (fs != null)
+          fs.Close ();
+      }
+
+    }
+  }
+}
