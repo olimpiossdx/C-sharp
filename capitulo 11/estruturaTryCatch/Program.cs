4fa356cc5a4e98c72e25ebf54342cc1765ec67c7
Olimpio <olimpiodx@gmail.com>
2020-03-10 20:12:51 -0300

    estrutura try-catch

---
+++ capitulo 11/estruturaTryCatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/*
+  Bloco try: contém o código que representa a execução normal do trecho de código que pode acarretar em uma execução
+  Bloco catch: contém o código a ser executado caso uam exceção ocorra. Deve ser especificado o tipo da exceção a ser tratada (upcasting é permitido)
+*/
+namespace estruturaTryCatch {
+  class Program {
+    static void Main (string[] args) {
+      int n1, n2, result;
+      try {
+        n1 = int.Parse (Console.ReadLine ());
+        n2 = int.Parse (Console.ReadLine ());
+        result = n1 / n2;
+        Console.WriteLine (result);
+      } catch (DivideByZeroException) {
+        Console.WriteLine ("Division by zero is not allowed");
+        //Console.WriteLine ("Error: " + e.Message);
+      } catch (FormatException e) {
+        //Console.WriteLine ("Division by zero is not allowed");
+        Console.WriteLine ("Format error: " + e.Message);
+      }
+
+    }
+  }
+}
